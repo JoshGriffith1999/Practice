b082881b79752cea599442000b1f80720bf7ee8e
JoshGriffith1999 <45469632+JoshGriffith1999@users.noreply.github.com>
2019-06-22 22:54:05 -0500

    added all functions needed for the binary search trees to work
    added all functions needed for the binary search trees to work

--- Practice/Binary_Trees.hpp
+++ Practice/Binary_Trees.hpp
@@ -8,8 +8,41 @@ class tree{
 	
 	private:
 		T tree[SIZE];
-		void 
-	
+		void addChildHelper();
+		void preOderPrintHelper();
+		void postOrderPrintHelper();
+		void inOrderPrintHelper();
+		
+		public:
+		tree();
+		~tree();
+		
+		void addChild(T);
+		void preOderPrint();
+		void postOrderPrint();
+		void inOrderPrint();
+		void search(T);
 }
 
+void tree<T>::addChildHelper(){}
+
+void tree<T>::preOderPrintHelper(){}
+
+void tree<T>::postOrderPrintHelper(){}
+
+void tree<T>::inOrderPrintHelper(){}
+
+tree<T>::tree(){}
+
+tree<T>::~tree(){}
+
+void tree<T>::addChild(T child){}
+
+void tree<T>::preOderPrint(){}
+
+void tree<T>::postOrderPrint(){}
+
+void tree<T>::inOrderPrint(){}
+
+void tree<T>::search(T){}
 #endif
