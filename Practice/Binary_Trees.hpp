6544e7a7f96cd7a9b0ac553a5f7460f4a6fb333c
JoshGriffith1999 <45469632+JoshGriffith1999@users.noreply.github.com>
2019-06-29 21:14:55 -0500

    updated everythying
    updated everythying

--- Practice/Binary_Trees.hpp
+++ Practice/Binary_Trees.hpp
@@ -1,6 +1,9 @@
 #ifndef Binary_Trees_hpp
 #define Binary_Trees_hpp
+
 #include<iostream>
+#include "Node.hpp"
+
 #define SIZE 100
 
 template<typename T>
@@ -8,10 +11,11 @@ class tree{
 	
 	private:
 		T tree[SIZE];
-		void addChildHelper();
-		void preOderPrintHelper();
-		void postOrderPrintHelper();
-		void inOrderPrintHelper();
+		void addChildHelper(int, T);
+		void preOderPrintHelper(int);
+		void postOrderPrintHelper(int);
+		void inOrderPrintHelper(int pos);
+		void searchHelper(int, T);
 		
 		public:
 		tree();
@@ -22,27 +26,119 @@ class tree{
 		void postOrderPrint();
 		void inOrderPrint();
 		void search(T);
+};
+
+template<typename T>
+tree<T>::tree(){
+	
+	int i = 0;
+	
+	for(i = 0; i < SIZE; i++){
+		
+		this->tree[i] = 0;		
+	}
+	
 }
 
-void tree<T>::addChildHelper(){}
+template<typename T>
+tree<T>::~tree(){
+	
+	int i = 0;
+	
+	for(i = 0; i < SIZE; i++){
+		
+		this->tree[i] = 0;		
+	}
+	
+}
 
-void tree<T>::preOderPrintHelper(){}
+template<typename T>
+void tree<T>::addChildHelper(int pos, T value){
+	
+	if(this->tree[pos] == 0)
+		this->tree[pos] = value;
+	
+	else if(value < this->tree[pos])
+		this->addChildHelper((2*pos)+1, value);
+	
+	else
+		this->addChildHelper((2*pos)+2, value);
+}
 
-void tree<T>::postOrderPrintHelper(){}
+template<typename T>
+void tree<T>::preOderPrintHelper(int pos){
+	
+	if(pos < SIZE && this->tree[pos] != 0){
+		cout << this->tree[pos] << endl;
+	
+	this->preOderPrintHelper((2 * pos) + 1);
+	this->preOderPrintHelper((2 * pos) + 2);
+}
+}
 
-void tree<T>::inOrderPrintHelper(){}
+template<typename T>
+void tree<T>::postOrderPrintHelper(int pos){
+	
+	if(pos < SIZE && this->tree[pos] != 0){
+	
+	this->preOderPrintHelper((2 * pos) + 1);
+		cout << this->tree[pos] << endl;
+		
+	this->preOderPrintHelper((2 * pos) + 2);
+		cout << this->tree[pos] << endl;
+}
+}
 
-tree<T>::tree(){}
+template<typename T>
+void tree<T>::inOrderPrintHelper(int pos){
+	
+	if(pos < SIZE && this->tree[pos] != 0){
+	
+	this->preOderPrintHelper((2 * pos) + 1);
+	this->preOderPrintHelper((2 * pos) + 2);
+	
+	cout << this->tree[pos] << endl;
+}
+template<typename T>
+void tree<T>::searchHelper(int pos, T value){
+	
+	if(value == this->tree[pos])
+		cout << "value found" << endl;
+	
+	else if(value < this->tree[pos])
+		this->searchHelper((2*pos)+1, value);
+	
+	else
+		this->searchHelper((2*pos)+2, value);
+}
 
-tree<T>::~tree(){}
+template<typename T>
+void tree<T>::addChild(T child){
+	
+	this->addChildHelper(0,child);
+}
 
-void tree<T>::addChild(T child){}
+template<typename T>
+void tree<T>::preOderPrint(){
+	
+	this->preOderPrintHelper(o)
+}
 
-void tree<T>::preOderPrint(){}
+template<typename T>
+void tree<T>::postOrderPrint(){
+	
+	this->postOrderPrintHelper(0);
+}
 
-void tree<T>::postOrderPrint(){}
+template<typename T>
+void tree<T>::inOrderPrint(){
+	
+	this->inOrderPrintHelper(0);
+}
 
-void tree<T>::inOrderPrint(){}
-
-void tree<T>::search(T){}
+template<typename T>
+void tree<T>::search(T value){
+	
+	this->searchHelper(0, value);
+}
 #endif
