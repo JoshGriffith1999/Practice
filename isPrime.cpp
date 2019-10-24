396ecf0e41655442044a79ce3dd5cb921d136b36
JoshGriffith1999 <45469632+JoshGriffith1999@users.noreply.github.com>
2019-10-24 14:25:26 -0500

    isPrime file
    Used this to solve a math question

---
+++ isPrime.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#define SIZE 1000000
+using namespace std;
+
+
+bool isPrime(int p){
+	
+	if(p % 3 != 0)
+		return false;
+	else if(p % 3 != 0)
+		return false;
+	else if(p % 5 != 0)
+		return false;
+	else if(p % 7 != 0)
+		return false;
+	else
+		return true;
+	
+	
+	
+	
+}
+
+int main(){
+	
+
+	
+	for(unsigned int i = 0; i < SIZE; i++){
+		
+		if(isPrime(i) && isPrime(i + 3))
+			cout << i << "" << i + 3 << endl;
+				
+		
+	}
+	
+	
+	cout << "Hello mark" << endl;
+	return 0;
+}
