38bf8c1ad18b0b121112b2693b57d2b65214f481
JoshGriffith1999 <45469632+JoshGriffith1999@users.noreply.github.com>
2019-12-19 22:44:23 -0600

    changes
    made changes to where I now only have one custom built math library

---
+++ mathTest.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include"myMath.hpp"
+
+using namespace std;
+
+
+int main(){
+	
+	float a = 5;
+	float b = 3;
+	float c = 2;
+	
+	cout << power_mod(c,b,a) << endl;
+	return 0;
+}
--- myMath.hpp
+++ myMath.hpp
@@ -5,7 +5,113 @@
 using namespace std;
 
 
-bool isPrime(int p){
+
+//Basic Algebra
+template<typename T>
+T squareRoot(T x){
+	
+	T prev,k = 0;
+        T kmax = 1000;
+        T s = 1;
+        for(k=0;k<kmax;k++)
+        {
+                prev = s;
+                s = (s + x/s)/2;
+                if(prev == s)
+                {
+                        break;
+                }
+        }
+        return s;
+}
+
+template<typename T>
+T square(T num){
+	T result;
+	
+	result = num * num;
+	
+	return result;
+}
+template<typename T>
+T cube(T num){
+	T result;
+	
+	result = num * num * num;
+	
+	return result;
+	
+}
+template<typename T>
+T power(T base, T exponent){
+	
+	T result = 1;
+	T counter;
+	
+	for(counter = 1; counter <= exponent; counter++){
+		
+		result = result * base;
+		
+		
+	}
+	return result;
+}
+template<typename T>
+T factorial(T x){
+	
+	T result = 1;
+	for(unsigned int k = 1; k < x; k++){
+		
+		result = result * k;
+		
+	}
+	
+	return result;
+}
+template<typename T>
+T mod(T x, T y){
+	
+	while(x >= y){
+		x -= y;
+	}
+	
+	return x;
+	
+}
+//Geometry
+template<typename T>
+T sin(T opposite, T hypotenuse){
+	
+	T result;
+	
+	result = opposite/hypotenuse;
+	
+	return result;
+	
+}
+template<typename T>
+T cos(T adjacent, T hypotenuse){
+	
+	T result;
+	
+	result = adjacent/hypotenuse;
+	
+	return result;
+}
+template<typename T>
+T tan(T opposite, T adjacent){
+	
+	T result;
+	
+	result = opposite/adjacent;
+	
+	return result;
+}
+
+
+//Discrete
+template<typename T>
+bool isPrime(T p){
 	
 	if(p % 2 != 0)
 		return false;
@@ -23,93 +129,24 @@ bool isPrime(int p){
 	
 }
 
-//Basic Algebra
-float squareRoot(float x){
+/*
+calls power function that way there is more code reuse
+*/
+template<typename T>
+T power_mod(T base, T exponent, T module){
 	
-	float prev,k = 0;
-        float kmax = 1000;
-        float s = 1;
-        for(k=0;k<kmax;k++)
-        {
-                prev = s;
-                s = (s + x/s)/2;
-                if(prev == s)
-                {
-                        break;
-                }
-        }
-        return s;
-}
-
-float square(float num){
-	float result;
-	
-	result = num * num;
-	
-	return result;
-}
-
-float cube(float num){
-	float result;
-	
-	result = num * num * num;
-	
-	return result;
+	return mod(power(base, exponent), module);
 	
 }
 
-float power(float base, float exponent){
+template<typename T>
+T gcd(T x, T y){
 	
-	float result = 1;
-	int counter;
-	
-	for(counter = 1; counter <= exponent; counter++){
-		
-		result = result * base;
-		
-		
-	}
-	return result;
-}
-
-float factorial(long float x){
-	
-	long float result = 1;
-	for(unsigned int k = 1; k < x; k++){
-		
-		result = result * k;
-		
-	}
-	
-	return result;
-}
-
-//Geometry
-float sin(float opposite, float hypotenuse){
-	
-	float result;
-	
-	result = opposite/hypotenuse;
-	
-	return result;
+	if(x == 0)
+		return y;
+	return gcd(y % x, x);
 	
 }
 
-float cos(float adjacent, float hypotenuse){
-	
-	float result;
-	
-	result = adjacent/hypotenuse;
-	
-	return result;
-}
 
-float tan(float opposite, float adjacent){
-	
-	float result;
-	
-	result = opposite/adjacent;
-	
-	return result;
-}
 #endif
