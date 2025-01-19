085ef58e9ff58df0a1ca3e183f5e4b6de24e6c03
Josh Griffith <josh.w.griffith@gmail.com>
2025-01-18 23:13:15 -0600

    Updated git command and java

--- java/Main.java
+++ java/Main.java
@@ -1,5 +1,10 @@
 public class Main {
+
+    public static void newMethod(  ){
+        System.out.println( "Hello, I am a method" );
+    }
     public static void main(String[] args){
         System.out.println("Hello lInux I am a java program!");
+        newMethod();
     }
 }
